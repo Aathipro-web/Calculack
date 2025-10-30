80dbebbe406ea9e15eddf05739c95c3512e92d11
Aathipro-web <aathistudio08@gmail.com>
2025-10-30 20:18:11 +0530

    first upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display")
+function appendToDisplay(input){
+display.value += input;
+}
+function deleteLastDigit() {
+    const display = document.getElementById('display');
+    display.value = display.value.slice(0, -1); // Remove the last digit
+}
+function clearDisplay(){
+display.value = "";
+}
+function calculate(){
+try{
+display.value = eval(display.value);
+}
+catch(error){
+display.value = "Error";
+}
+}
+function myFunction() {
+  alert("Error");
+}
